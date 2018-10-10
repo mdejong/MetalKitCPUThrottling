4fa60a80c0b5864f8b1d6ea8f3fa0e457f6dfaf5
Mo DeJong <mo@modejong.com>
2018-10-10 12:42:15 -0700

    Initial Commit

---
+++ MetalKitCPUThrottling/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MetalKitCPUThrottling
+//
+//  Created by Mo DeJong on 10/10/18.
+//  Copyright © 2018 HelpURock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MetalKitCPUThrottling/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MetalKitCPUThrottling
+//
+//  Created by Mo DeJong on 10/10/18.
+//  Copyright © 2018 HelpURock. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
